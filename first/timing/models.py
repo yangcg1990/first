f90bb3886f446dca7a9ca868cf9abd86c3e99d65
yangcg199 <yangcg199@163.com>
2018-07-06 09:27:04 +0800

    去掉时间差字段

--- first/timing/models.py
+++ first/timing/models.py
@@ -6,7 +6,7 @@ class Meeting(models.Model):
     start = models.DateTimeField(null=True, default=None, help_text='开始时间')
     end = models.DateTimeField(null=True, default=None, help_text='结束时间')
     state = models.CharField(max_length=32, default='未开始', help_text='状态')
-    dif = models.DateTimeField(null=True, default=None, help_text='时间差')
+    # dif = models.DateTimeField(null=True, default=None, help_text='时间差')
 
     def save_me(self, request):
         start = request.POST.get('start', None)
